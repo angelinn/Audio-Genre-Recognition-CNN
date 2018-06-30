df996619c3ea6a1824edda4750f841018b2c2eca
angelinn <angelin.nedelchev@outlook.com>
2018-06-30 20:20:35 +0300

    Create simple CNN model.

---
+++ train.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+print('Importing libraries...')
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+import tflearn
+warnings.resetwarnings()
+
+from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+
+print('Done')
+
+def create_model(imageSize, classesLength):
+    print('in')
+    convnet = input_data(shape=[None, imageSize, imageSize, 1])
+
+    convnet = conv_2d(convnet, 64, 2, activation='elu', weights_init='Xavier')
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 128, 2, activation='elu', weights_init="Xavier")
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 256, 2, activation='elu', weights_init="Xavier")
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = conv_2d(convnet, 512, 2, activation='elu', weights_init="Xavier")
+    convnet = max_pool_2d(convnet, 2)
+
+    convnet = fully_connected(convnet, 1024, activation='elu')
+    convnet = dropout(convnet, 0.5)
+
+    convnet = fully_connected(convnet, classesLength, activation='softmax')
+    convnet = regression(convnet, optimizer='rmsprop', loss='categorical_crossentropy')
+
+    model = tflearn.DNN(convnet)
+    return model
