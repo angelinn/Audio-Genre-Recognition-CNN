b6aed8cb5f794b29a64d7a0e276ce30fffaed056
angelinn <angelin.nedelchev@outlook.com>
2018-07-01 11:09:29 +0300

    Train model.

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from train import create_model
 from tools import SPECTROGRAM_SLICES_PATH
 from tools import get_dataset
 import os
+import random
+import string
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("mode", help="Trains or tests the CNN", nargs='+', choices=["train","test","slice"])
+args = parser.parse_args()
 
 filesPerGenre = 1000
 SLICE_SIZE = 128
@@ -21,4 +28,17 @@ print("Creating model...")
 model = create_model(SLICE_SIZE, genres_length)
 print('Model created')
 
-train_X, train_y, validation_X, validation_y = get_dataset(filesPerGenre, genres, SLICE_SIZE, validation_ratio, test_ratio, mode="train")
+if "train" in args.mode:
+    train_X, train_y, validation_X, validation_y = get_dataset(filesPerGenre, genres, SLICE_SIZE, validation_ratio, test_ratio, mode="train")
+    run_id = "MusicGenres - "+str(128)+" "+''.join(random.SystemRandom().choice(string.ascii_uppercase) for _ in range(10))
+
+    print('[+] Training model...')
+    model.fit(train_X, train_y, n_epoch=20, batch_size=128, shuffle=True, validation_set=(validation_X, validation_y), snapshot_step=100, show_metric=True, run_id=run_id)
+    print("    Model trained! ✅")
+
+    print("[+] Saving the weights...")
+    model.save('musicDNN.tflearn')
+    print("[+] Weights saved! ✅💾")
+
+if "test" in args.mode:
+    print('train')
