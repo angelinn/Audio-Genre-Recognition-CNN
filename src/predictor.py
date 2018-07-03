bf9c03fe460f210d2e99a399ac408d1242e3dc1f
angelinn <angelin.nedelchev@outlook.com>
2018-07-03 17:27:52 +0300

    Create numpy array from user input mp3.

--- src/predictor.py
+++ src/predictor.py
@@ -3,11 +3,18 @@ import os
 import sys
 import spectrogram as sp
 
+from train import create_model
+from tools import convert_slices_to_array
+
+SLICE_SIZE = 128
+
 print("Reading music file {}...".format(sys.argv[1]))
 
 folder_name = os.path.dirname(sys.argv[1])
 file_name = os.path.basename(sys.argv[1])
 
-sp.create_spectrogram(folder_name, file_name, 'user.mp3')
-sp.slice_spectrogram(sys.argv[1], 128)
+script_path = os.path.realpath(__file__)
+sp.create_spectrogram(folder_name, file_name, script_path, 'spectrogram.png')
+sp.slice_spectrogram(folder_name, file_name, 128, scripts_path, 'slice')
 
+x, y = convert_slices_to_array(script_path, genre, 128)
