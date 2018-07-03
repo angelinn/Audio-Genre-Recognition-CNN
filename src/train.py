3ed182bee267b490d121b87ce858f67ab0a1ab6a
angelinn <angelin.nedelchev@outlook.com>
2018-07-03 17:27:36 +0300

    Swap elu for relu and rmsprop for adam.

--- src/train.py
+++ src/train.py
@@ -4,7 +4,6 @@ print('Importing libraries...')
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 import tflearn
-warnings.resetwarnings()
 
 from tflearn.layers.conv import conv_2d, max_pool_2d
 from tflearn.layers.core import input_data, dropout, fully_connected
@@ -15,23 +14,23 @@ print('Done')
 def create_model(image_size, classes_length):
     convnet = input_data(shape=[None, image_size, image_size, 1])
 
-    convnet = conv_2d(convnet, 64, 2, activation='elu', weights_init='Xavier')
+    convnet = conv_2d(convnet, 64, 2, activation='relu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 128, 2, activation='elu', weights_init="Xavier")
+    convnet = conv_2d(convnet, 128, 2, activation='relu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 256, 2, activation='elu', weights_init="Xavier")
+    convnet = conv_2d(convnet, 256, 2, activation='relu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 512, 2, activation='elu', weights_init="Xavier")
+    convnet = conv_2d(convnet, 512, 2, activation='relu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
-
-    convnet = fully_connected(convnet, 1024, activation='elu')
+    
+    convnet = fully_connected(convnet, 1024, activation='relu')
     convnet = dropout(convnet, 0.5)
 
     convnet = fully_connected(convnet, classes_length, activation='softmax')
-    convnet = regression(convnet, optimizer='rmsprop', loss='categorical_crossentropy')
+    convnet = regression(convnet, optimizer='adam', loss='categorical_crossentropy')
 
     model = tflearn.DNN(convnet)
     return model
