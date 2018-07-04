58d22f42e4bdefc845bf36c19a2d7eb1eea514f9
angelinn <angelin.nedelchev@outlook.com>
2018-07-04 11:31:34 +0300

    Set optimizer to rmsprop and activation to elu.

--- src/train.py
+++ src/train.py
@@ -14,23 +14,23 @@ print('Done')
 def create_model(image_size, classes_length):
     convnet = input_data(shape=[None, image_size, image_size, 1])
 
-    convnet = conv_2d(convnet, 64, 2, activation='relu', weights_init='Xavier')
+    convnet = conv_2d(convnet, 64, 2, activation='elu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 128, 2, activation='relu', weights_init='Xavier')
+    convnet = conv_2d(convnet, 128, 2, activation='elu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 256, 2, activation='relu', weights_init='Xavier')
+    convnet = conv_2d(convnet, 256, 2, activation='elu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
 
-    convnet = conv_2d(convnet, 512, 2, activation='relu', weights_init='Xavier')
+    convnet = conv_2d(convnet, 512, 2, activation='elu', weights_init='Xavier')
     convnet = max_pool_2d(convnet, 2)
     
-    convnet = fully_connected(convnet, 1024, activation='relu')
+    convnet = fully_connected(convnet, 1024, activation='elu')
     convnet = dropout(convnet, 0.5)
 
     convnet = fully_connected(convnet, classes_length, activation='softmax')
-    convnet = regression(convnet, optimizer='adam', loss='categorical_crossentropy')
+    convnet = regression(convnet, optimizer='rmsprop', loss='categorical_crossentropy')
 
-    model = tflearn.DNN(convnet)
+    model = tflearn.DNN(convnet, tensorboard_verbose=3, checkpoint_path='checkpoints', best_checkpoint_path='best')
     return model
