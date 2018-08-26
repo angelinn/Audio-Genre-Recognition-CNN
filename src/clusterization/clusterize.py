a61d5f4d8cc07340636733a8341fa2fe0ef84ade
angelinn <angelin.nedelchev@outlook.com>
2018-08-26 14:37:31 +0300

    Add sample scikit KMeans code.

--- src/clusterization/clusterize.py
+++ src/clusterization/clusterize.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import numpy as np
+from sklearn.cluster import KMeans
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from tools import get_image_data
@@ -19,6 +21,12 @@ def main():
     data = load_slices()
     print(data)
     print(len(data))
+
+    X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
+    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
+    print(kmeans.labels_)
+    print(kmeans.predict([[0, 0], [4, 4]]))
+    print(kmeans.cluster_centers_)
 
 if __name__ == '__main__':
     main()
