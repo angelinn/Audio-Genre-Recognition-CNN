36ed674ff5e70614243efa65c340591a2acdd9a4
angelinn <angelin.nedelchev@outlook.com>
2018-08-28 16:08:16 +0300

    Describe combiner task.

---
+++ src/combining/combiner.py
@@ -0,0 +1,6 @@
+# 1. Analyze 10 songs of each genre
+# 2. Find the odd results - e.g slices that were not correctly recognised
+# 3. Combine slices from different songs from the same genre
+# 4. Find the music parts corresponding to the slices
+# 5. Fetch the parts from the mp3 files and glue 'em in a new audio file
+
