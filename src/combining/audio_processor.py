f9b2adf5587ccab43c946cbc597ac63a4590ade5
angelinn <angelin.nedelchev@outlook.com>
2018-08-30 08:19:59 +0300

    Retrieve song portion the correct way.

--- src/combining/audio_processor.py
+++ src/combining/audio_processor.py
@@ -8,19 +8,24 @@ SCRIPT_PATH = os.path.dirname(SCRIPT_PATH) + "\\"
 def create_audio(audio_files):
     songs = []
     piece = AudioSegment.empty()
-    duration = 1 * 1000
-    start = 30 * 1000
+    duration = 1* 1000
 
     for slice in audio_files:
+        slice_index = audio_files[1]
+        seconds_from_start = make_seconds(slice_index)
+
         print('Loading {} ...'.format(slice[0]))
         song = AudioSegment.from_mp3(slice[0])
         songs.append(song)
 
-        piece = piece + song[start:start + duration]
+        piece = piece + song[seconds_from_start:seconds_from_start + duration]
 
     piece.export(SCRIPT_PATH + 'mashup.mp3', format='mp3')
 
     print('Done')   
+
+def make_seconds(slice_index):
+    return slice_index * 1000
 
 def main():
     with open(SCRIPT_PATH + 'slices.bin', 'rb') as slfile:
